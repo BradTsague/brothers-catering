951f792057e9d5aadbe7150366bc94658f83b38b
Dimitri Njila <dimitrinjila45@gmail.com>
2026-01-14 17:06:23 -0500

    added vercel analytics

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const mobileMenu = document.getElementById("mobile-menu");
 const navMenu = document.querySelector(".hero-nav");
+import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from '@vercel/speed-insights';
+injectSpeedInsights();
+inject();
 
 mobileMenu.addEventListener("click", () => {
   mobileMenu.classList.toggle("active");
